c02e501f6555697eb546ad7bdd1a97a521a3a27c
EC2 Default User <ec2-user@ip-172-31-69-152.ec2.internal>
2020-10-18 07:22:57 +0000

    finish 3

---
+++ Bookers2/app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  attachment :profile_image
+  
+  has_many :books, dependent: :destroy
+  
+  has_many :favorites, dependent: :destroy
+  
+  has_many :comments, dependent: :destroy
+  
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  validates :email, presence: true
+end
---
+++ Bookers2/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'top_page#top'
+  get 'home/about' => 'top_page#about'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+  resource :favorites, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
+  end
+end
